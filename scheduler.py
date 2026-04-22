eced0062c9245f1edf8b37e113f06b5761a3e4b3
Sivaharish J <sivaharish@cloudrevelinnovation.com>
2026-04-22 09:13:03 +0530

    Time updated

--- scheduler.py
+++ scheduler.py
@@ -9,15 +9,15 @@ weekday = now.weekday()
 # Only Mon-Sat (0–5)
 if weekday < 6:
 
-    # Checkin → 3:20 UTC
-    if now.hour == 3 and now.minute == 20:
+    # Checkin → 8:50 AM to 8:55 AM IST (3:20 to 3:25 UTC)
+    if now.hour == 3 and 43 <= now.minute <= 45:
         print("Running CHECKIN")
-        subprocess.run(["python3", "/app/staffpulse_automation.py", "checkin"])
+        subprocess.run(["python3", "staffpulse_automation.py", "checkin"])
 
-    # Checkout → 13:20 UTC
-    elif now.hour == 13 and now.minute == 35:
+    # Checkout → 6:50 PM to 6:55 PM IST (13:20 to 13:25 UTC)
+    elif now.hour == 13 and 20 <= now.minute <= 25:
         print("Running CHECKOUT")
-        subprocess.run(["python3", "/app/staffpulse_automation.py", "checkout"])
+        subprocess.run(["python3", "staffpulse_automation.py", "checkout"])
 
     else:
         print("No task now:", now)
